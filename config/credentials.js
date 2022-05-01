37722da7c76e5a07ecaa6d5bfa870c420ffd5bff
Despoinam <despinan0@gmail.com>
2022-05-01 12:16:57 +0300

    read all files

--- config/credentials.js
+++ config/credentials.js
@@ -2,8 +2,8 @@ var credentials = {
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "softeng",
-    database: "actual_total_load"
+    password: "",
+    database: "Actual_Total_Load"
 }
 
 module.exports = credentials;
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mysql       = require('mysql');
 const bodyParser  = require('body-parser');
 const fs          = require('fs')
 const fastcsv     = require("fast-csv");
+const path = require('path');
 const app         = express();
 const credentials = require('./config/credentials')
 const connection     = require('./config/connect')
@@ -21,52 +22,68 @@ app.listen(port, () =>{
 var con = mysql.createConnection(credentials);
 connection(con);
 
-const fileName ='2022_01_01_01_ActualTotalLoad6.1.A.csv';
+const directoryPath = 'C:/Users/30694/Documents/GitHub/SoftEng2021_/docs/test'; //replace, to evala na treksei me 4 arxeia giati emene apo mnimi
+//console.log(directoryPath);
+//const fileName ='C:\\Users\\30694\\Documents\\ATL\\2022_01_01_01_ActualTotalLoad6.1.A.csv';
 const { parse } = require("csv-parse");
 const { CLIENT_IGNORE_SPACE } = require('mysql/lib/protocol/constants/client');
 
-fs.createReadStream(fileName)
-  .pipe(parse({ delimiter: "\t", from_line: 2 }))
-  .on("data", function (row) {
-      // console.log(`table ${row[5]} created`)
-      
-    if(row[3] === 'CTY'){
-      var name = row[5].replace("-","_");
-    //console.log(row[5].replace("-","_"));
-      con.query(totalload(name), function(err) {
-        if (err) {
-        throw err;
-        }
-      });
-      let insertStatement = 
-          `INSERT INTO ${name} values(?,?,?,?,?)
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    //console.log(directoryPath);
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+        const filePath = path.join(directoryPath, file);
+        fs.createReadStream(filePath)
+            .pipe(parse({ delimiter: "\t", from_line: 2 }))
+            .on("data", function (row) {
+                // console.log(`table ${row[5]} created`)
+
+                if(row[3] === 'CTY'){
+                    var name = row[5].replace("-","_");
+                    //console.log(row[5].replace("-","_"));
+                    con.query(totalload(name), function(err) {
+                        if (err) {
+                            throw err;
+                        }
+                    });
+                    let insertStatement =
+                        `INSERT INTO ${name} values(?,?,?,?,?)
            ON DUPLICATE KEY UPDATE UpdateTime = ?`;
-          var items = [row[0],row[1],row[5],row[6],row[7],row[7]];
-          /*Inserting data of current row
-          into database*/
-          con.query(insertStatement, items, 
-              (err, results, fields) => {
-              if(err) throw err;
-              // if (err.code === 'ER_DUP_ENTRY' || err.erno == 1062) {
-              //     let sql = `select * from ${name}`;
-              //     con.query(sql,function(err,res) {
-              //       if(err) throw err;
-              //       // console.log(res);
-              //       let newquery = `UPDATE ${name} 
-              //       SET (UpdateTime) values(?)`;
-              //       var update = row[7];
-              //       con.query(newquery,update,function(err) {
-              //         if(err) throw err;
-              //       })
-              //     })
-              // }
-          });
-        }
-    })
-  
-  .on("end", function () {
-    console.log("finished");
-  })
-  .on("error", function (error) {
-    console.log(error.message);
-  });
+                    var items = [row[0],row[1],row[5],row[6],row[7],row[7]];
+                    /*Inserting data of current row
+                    into database*/
+                    con.query(insertStatement, items,
+                        (err, results, fields) => {
+                            if(err) throw err;
+                            // if (err.code === 'ER_DUP_ENTRY' || err.erno == 1062) {
+                            //     let sql = `select * from ${name}`;
+                            //     con.query(sql,function(err,res) {
+                            //       if(err) throw err;
+                            //       // console.log(res);
+                            //       let newquery = `UPDATE ${name}
+                            //       SET (UpdateTime) values(?)`;
+                            //       var update = row[7];
+                            //       con.query(newquery,update,function(err) {
+                            //         if(err) throw err;
+                            //       })
+                            //     })
+                            // }
+                        });
+                }
+            }).on("end", function () {
+               console.log("file finished");
+             })
+             .on("error", function (error) {
+               console.log(error.message);
+            });
+    });
+});
+
+
+
+
+
