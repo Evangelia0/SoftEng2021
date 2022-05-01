0fe072022ca846f5124298bd420f95806cbdf85f
Evangelia0 <56823552+Evangelia0@users.noreply.github.com>
2022-05-01 09:38:13 +0300

    Add files via upload

--- config/queries.js
+++ config/queries.js
@@ -1,10 +1,13 @@
-const totalload = `create table if not exists TotalLoad(
-    DateTime TIMESTAMP primary key,
-    ResolutionCode VARCHAR(4),
-    AreaName VARCHAR(6),
-    TotalLoadValue NUMERIC(10,3),
-    UpdateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-      )`;
-//const change = `drop table TotalLoad`;
+const totalload = (name) => {
+  let query = 
+    `create table if not exists ${name}(
+        DateTime TIMESTAMP,
+        ResolutionCode VARCHAR(4),
+        MapCode VARCHAR(10),
+        TotalLoadValue NUMERIC(10,3),
+        UpdateTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        PRIMARY KEY (DateTime, MapCode,UpdateTime)
+          )`
+      return query;
+};
 module.exports = totalload;
-//module.exports = change;
