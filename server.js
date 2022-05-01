724a57ccf03a1595034f37214479b8f0104967fc
Evangelia0 <56823552+Evangelia0@users.noreply.github.com>
2022-05-01 09:37:44 +0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,14 +1,13 @@
 const express     = require('express');
 const mysql       = require('mysql');
 const bodyParser  = require('body-parser');
-const credentials = require('./config/credentials')
-const totalload   = require('./config/queries')
-var Iconv         = require('iconv-lite');
 const fs          = require('fs')
 const fastcsv     = require("fast-csv");
 const app         = express();
+const credentials = require('./config/credentials')
+const connection     = require('./config/connect')
+const totalload   = require('./config/queries')
 const port        = 8000;
-const languageEncoding = require("detect-file-encoding-and-language");
 //initialize app as an instance of express, the framework
 
 app.use(bodyParser.urlencoded({extended:true}));
@@ -18,56 +17,43 @@ app.listen(port, () =>{
     console.log('We are live on port ' + port);
 });
 
+//establish a connection
 var con = mysql.createConnection(credentials);
-
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
-    con.query("CREATE DATABASE IF NOT EXISTS Actual_Total_Load", function (err, result) {
-      if (err) throw err;
-      console.log("Database created");
-    });
-    con.query(totalload, function(err) {
-      if (err) {
-      throw err;
-      }
-      console.log("table total load created")
-      })
-    });
+connection(con);
 
 const fileName ='2022_01_01_01_ActualTotalLoad6.1.A.csv';
 const { parse } = require("csv-parse");
-const csv = require('csvtojson');
-csv({delimiter:'\t'})
-    .fromFile(fileName)
-    .then((json)=>{
-      console.log(json);
-      let insertStatement = 
-        `INSERT INTO totalload values(?, ?, ?, ?,?)`;
-        var items = [json];
-        // Inserting data of current row
-        // into database
-        con.query(insertStatement, items, 
-            (err, results, fields) => {
-            if (err) {
-                console.log(err);
-            }
-        });
-    });
 
-// fs.createReadStream("2022_01_01_01_ActualTotalLoad6.1.A.csv")
-//   .pipe(parse({ delimiter: ",", from_line: 1 }))
-//   .on("data", function (row) {
-//     var DateTime = row.DateTime,
-//     ResolutionCode = row["ResolutionCode"],
-//     AreaName = row["AreaName"],
-//     TotalLoadValue = row["TotalLoadValue"]
-//     UpdateTime = row["UpdateTime"]
-//     console.log(DateTime);
-//   })
-//   .on("end", function () {
-//     console.log("finished");
-//   })
-//   .on("error", function (error) {
-//     console.log(error.message);
-//   });
+fs.createReadStream(fileName)
+  .pipe(parse({ delimiter: "\t", from_line: 2 }))
+  .on("data", function (row) {
+    let name = row[5].replace("-","_");
+    //console.log(row[5].replace("-","_"));
+    con.query(totalload(name), function(err) {
+      if (err) {
+      throw err;
+      }
+      console.log(`table ${row[5]} created`)
+      });
+    if(row[3] === 'CTY'){
+      console.log(row[3])
+      let insertStatement = 
+          `INSERT INTO ${name} values(?,?,?,?,?)`;
+          var items = [row[0],row[1],row[5],row[6],row[7]];
+          /*Inserting data of current row
+          into database*/
+          con.query(insertStatement, items, 
+              (err, results, fields) => {
+              if (err.code === 'ER_DUP_ENTRY' || err.erno == 1062) {
+                  if(row[7])
+              }
+          });
+        }
+    })
+  
+  .on("end", function () {
+    console.log("finished");
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
